a3a10bf187d672c339cb82dda6e447c8021b0f91
Prakash Jangid <kanekiken8333@gmail.com>
2026-04-21 11:36:48 +0530

    health route add

--- src/app.js
+++ src/app.js
@@ -8,6 +8,10 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(morgan("dev"))
 
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
 app.use("/api/auth", authRoutes);
 app.use("/api/chat", chatRoutes);
 app.use("/api/status", statusRoutes);
